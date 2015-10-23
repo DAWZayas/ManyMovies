3447a54bd1891d681bb6d023a9e9c3db8c958c65
gotrecillo <sergio.panadero.perez@gmail.com>
2015-10-23 18:39:29 +0200

    boilerplate for the application

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default React.createClass({
+  render: function() {
+    return <div>
+      <a className="btn btn-info" href="#">Hi World!</a>
+    </div>
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css!autoprefixer?browsers=last 2 versions'
+    },
+    { test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000' }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
