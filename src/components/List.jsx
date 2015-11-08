ade1a7cd9322edd92eadc3a3f3cf4896e36b9968
AlexiaTorres <maikuxy20@gmail.com>
2015-11-08 10:41:17 +0100

    components: semiesqueleto List y ListElement

---
+++ src/components/List.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default React.createClass({
+
+//solo borrar listas en las listas que custom true
+  render: function() {  
+    return (
+      <div className="main">
+        <h2> getTitle(id) </h2>
+        <ul>
+          {
+          //listas y su respectivo botón de borrar y editar, solo las custom true
+          }
+        </ul>
+        //crear nuevas listas
+      </div>
+    );
+  },
+
+  handleAddClick{},
+
+  handleEditList{},
+
+  handleRemoveClick{},
+
+});
