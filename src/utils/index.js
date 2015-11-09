9e9b2dcf96fc9379f09b4d55fc7289a1fd89e798
gotrecillo <sergio.panadero.perez@gmail.com>
2015-11-09 22:03:58 +0100

    slugText tests, moved tests from getSlug to slugText

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,9 @@ import uuid from 'node-uuid';
 export const getId = () => uuid.v1();
 
 //TODO
-//     (collection, id) => string
+//                            (collection, id) => string
 export const getSlug = (collection, title, id) => id ? title : title;
+
+export const slugText = string => string;
 
 export const allTrim = string => string;
--- test/utils/utils_spec.js
+++ test/utils/utils_spec.js
@@ -1,41 +1,113 @@
 import { expect } from 'chai';
-import { getSlug, getId, allTrim } from '../../src/utils';
+import { getSlug, getId, allTrim, slugText } from '../../src/utils';
 import defaultLists from '../../src/utils/examples';
 
-describe('getSlug', () => {
+describe('utils tests', () => {
+
+  describe.skip('slugText tests', () => {
+    it('will change to lower case the letters', () => {
+      const text = 'FoO';
+      const slug = slugText(text);
+
+      expect(slug).to.equal('foo');
+    });
+
+    it('will convert ·/_,:;. characters and spaces to hypens ( - )', () => {
+      const expected = 'foo-foo';
+      const text = 'foo·foo';
+      const text2 = 'foo/foo';
+      const text3 = 'foo_foo';
+      const text4 = 'foo,foo';
+      const text5 = 'foo:foo';
+      const text6 = 'foo;foo';
+      const text7 = 'foo.foo';
+      const text8 = 'foo-foo';
+      const text9 = 'foo foo';
+
+      const slug = slugText(text);
+      const slug2 = slugText(text2);
+      const slug3 = slugText(text3);
+      const slug4 = slugText(text4);
+      const slug5 = slugText(text5);
+      const slug6 = slugText(text6);
+      const slug7 = slugText(text7);
+      const slug8 = slugText(text8);
+      const slug9 = slugText(text9);
+
+      expect(slug).to.equal(expected);
+      expect(slug2).to.equal(expected);
+      expect(slug3).to.equal(expected);
+      expect(slug4).to.equal(expected);
+      expect(slug5).to.equal(expected);
+      expect(slug6).to.equal(expected);
+      expect(slug7).to.equal(expected);
+      expect(slug8).to.equal(expected);
+      expect(slug9).to.equal(expected);
+    });
+
+    it('will remove accents', () => {
+      const text = 'àáäâ';
+      const text2 = 'èéëê';
+      const text3 = 'ìíïî';
+      const text4 = 'òóöô';
+      const text5 = 'ùúüû';
+
+      const slug = slugText(text);
+      const slug2 = slugText(text2);
+      const slug3 = slugText(text3);
+      const slug4 = slugText(text4);
+      const slug5 = slugText(text5);
+
+      expect(slug).to.equal('aaaa');
+      expect(slug2).to.equal('eeee');
+      expect(slug3).to.equal('iiii');
+      expect(slug4).to.equal('oooo');
+      expect(slug5).to.equal('oooo');
+    });
+
+    it('will change ç and ñ characters', () => {
+      const text = 'ç';
+      const text2 = 'ñ';
+
+      const slug = slugText(text);
+      const slug2 = slugText(text2);
+
+      expect(slug).to.equal('c');
+      expect(slug2).to.equal('n');
+    });
+
+    it('will delete another symbols', () => {
+      const text = '*/+?¿)(&%$"!|@#¬[]{}´><\'\\';
+      const slug = slugText(text);
+      expect(slug).to.equal('');
+    });
+
+    it('will combine multiple hypens', () => {
+      const text = 'foo·/_, :;.foo';
+      const slug = slugText(text);
+      expect(slug).to.equal('foo-foo');
+    });
+
+    it('will remove starting and ending hypens', () => {
+      const text = '-foo-foo-';
+      const text2 = '·/_, :;.';
+
+      const slug = slugText(text);
+      const slug2 = slugText(text2);
+      expect(slug).to.equal('foo-foo');
+      expect(slug2).to.equal('');
+    });
+  });
 
   describe.skip('getSlug tests', () => {
 
     it('will be the title formated', () => {
       const title = 'foo';
       const id = getId();
+      const expected = slugText(title);
       const slug = getSlug(defaultLists, title, id);
 
-      const title2 = 'ñ';
-      const slug2 = getSlug(defaultLists, title2, id);
-
-      const title3 = 'foo foo';
-      const slug3 = getSlug(defaultLists, title3, id);
-
-      const title4 = 'Bar';
-      const slug4 = getSlug(defaultLists, title4, id);
-
-      const title5 = 'Baz*/+?¿)(&%$·"!|@#¬[ ]{}´;:,.-_><\'\\Baz';
-      const slug5 = getSlug(defaultLists, title5, id);
-
-      const title6 = 'çaz';
-      const slug6 = getSlug(defaultLists, title6, id);
-
-      const title7 = 'àáâä';
-      const slug7 = getSlug(defaultLists, title7, id);
-
-      expect(slug).to.equal('foo');
-      expect(slug2).to.equal('n');
-      expect(slug3).to.equal('foo-foo');
-      expect(slug4).to.equal('bar');
-      expect(slug5).to.equal('baz-baz');
-      expect(slug6).to.equal('caz');
-      expect(slug7).to.equal('aaaa');
+      expect(slug).to.equal(expected);
     });
 
     it('will be the id if the title can\'t be formated', () => {
