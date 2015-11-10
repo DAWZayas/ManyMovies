6ca5ec320692aa0b3984967264b58bae75d58be9
gotrecillo <sergio.panadero.perez@gmail.com>
2015-11-10 09:18:42 +0100

    linting rules added

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,7 +11,7 @@ export const allTrim = string => string.trim().replace(/\s+/g, ' ');
 export function getSlug (collection, title, id){
     let slug = slugText(title);
     let newTitle;
-    for(id in collection){
+    for (id in collection){
        id.title === slug ? newTitle = slug + '-' + id : newTitle = slug;
     }
   return newTitle;
