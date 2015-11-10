5d6e1c20dbe0d4e59dcbc38f451266b69be685ee
AlexiaTorres <maikuxy20@gmail.com>
2015-11-10 10:27:11 +0100

    working on slug text

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,17 +2,19 @@ import uuid from 'node-uuid';
 
 export const getId = () => uuid.v1();
 
-//TODO
-//                            (collection, id) => string
-export const slugText = string => string;
+export const slugText = string => string.toLowerCase()
+                                        .replace(/ñ/gi, 'ñ')
+                                        .replace(/ç/gi, 'c')
+                                        .trim().replace(/\s+/g, '');
 
 export const allTrim = string => string.trim().replace(/\s+/g, ' ');
 
 export function getSlug (collection, title, id){
-    let slug = slugText(title);
-    let newTitle;
-    for(id in collection){
-       id.title === slug ? newTitle = slug + '-' + id : newTitle = slug;
-    }
-  return newTitle;
+  let slug = slugText(title);
+  for(let item in collection){
+     if(collection.hasOwnProperty(item)){
+     	slug = (item.slug === slug) ? `${slug}-${id}` : slug;
+     }
+  }
+  return slug;
 }
