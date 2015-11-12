584b5bfd0571dadafd3c05feb448675ae29f0cae
AlexiaTorres <maikuxy20@gmail.com>
2015-11-12 23:02:01 +0100

    MovieDetails design and link button for add the movie to lists

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,39 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Card = require('material-ui/lib/card/card');
-const CardActions = require('material-ui/lib/card/card-actions');
-const CardHeader = require('material-ui/lib/card/card-header');
 const CardMedia = require('material-ui/lib/card/card-media');
 const CardText = require('material-ui/lib/card/card-text');
 const CardTitle = require('material-ui/lib/card/card-title');
-const FlatButton = require('material-ui/lib/flat-button');
 
 export default React.createClass({
   render() {
     return (
       <Card>
-        <CardHeader title="Title"/>
         <CardMedia className="film-cover" overlay={
-           <CardTitle title="Title">
-            <li className="post-icon">
-             <button className="post-collections-icon"> <i className="material-icons">collections_bookmark</i> </button>
-             <button className="post-history-icon"> <i className="material-icons">history</i> </button>
-             <button className="post-list-icon"> <i className="material-icons">playlist_add</i> </button>
-            </li>
-          </CardTitle>
-          }>
+           <CardTitle title="Minions" />  }>
           <img src="https://walter.trakt.us/images/movies/000/130/970/posters/thumb/baa71aa408.jpg"/>
         </CardMedia>
-        <CardActions className="card-actions">
-         <li> <FlatButton label="Add to Collection"/> </li>
-         <li> <FlatButton label="Add to history"/> </li>
-         <li> <FlatButton label="Add to List"/> </li>
-        </CardActions>
+             <CardMedia className="film-cover" upperlay={
+              <CardTitle/> }>
+             <li className="post-icon">
+             <button className="post-collections-icon"> <i className="material-icons">collections_bookmark</i> </button>
+             <button className="post-history-icon"> <i className="material-icons">history</i> </button>
+            <Link to="/lists"> <button className="post-list-icon"> <i className="material-icons">playlist_add</i>  </button></Link>
+            </li>
+            </CardMedia>
         <CardText className="card-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        Minions Stuart, Kevin and Bob are recruited by Scarlet Overkill, a super-villain who, alongside her inventor husband Herb, hatches a plot to take over the world.
         </CardText>
       </Card>
     );
