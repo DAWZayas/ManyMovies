a9ba2bfc697fce57ed194fc99bf33cb312f74fd8
martincasesalexandra <martincasesalexandra@gmail.com>
2015-11-13 00:25:32 +0100

    added app bar

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
+import AppBar from 'material-ui/lib/app-bar';
+import IconButton from 'material-ui/lib/icon-button';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+//import Menu from 'material-ui/lib/menus/menu';
+const injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
 
 export default class App extends Component {
 
@@ -8,9 +15,22 @@ export default class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'orange'
+    };
     return (
       <div>
-        <h1>Welcome to my App</h1>
+        <AppBar title="ManyMovies"
+                style={style}
+                iconElementLeft={
+                  <IconMenu openDirection="bottom-right" iconButtonElement={<IconButton  iconClassName="glyphicon glyphicon-align-justify"/>}>
+                        <MenuItem primaryText="Lists" />
+                        <MenuItem primaryText="History" />
+                        <MenuItem primaryText="Collections" />
+                        <MenuItem primaryText="Movies" />
+                  </IconMenu>
+        }/>
+
         <Link to="/lists">Show Lists</Link>
         {this.props.children}
       </div>
