e5dac290ec0b1380c7621f72652c23cb0b3ab219
gotrecillo <sergio.panadero.perez@gmail.com>
2015-11-16 10:40:28 +0100

    manual mergin last commits

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,7 +4,7 @@ import AppBar from 'material-ui/lib/app-bar';
 import IconButton from 'material-ui/lib/icon-button';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 import IconMenu from 'material-ui/lib/menus/icon-menu';
-const injectTapEventPlugin = require("react-tap-event-plugin");
+import injectTapEventPlugin from "react-tap-event-plugin";
 injectTapEventPlugin();
 
 export default class App extends Component {
@@ -20,24 +20,22 @@ export default class App extends Component {
       <div>
         <AppBar title="ManyMovies"
                 style={style}
-<<<<<<< HEAD
                 showMenuIconButton={false}
                 iconElementRight={
-                  <IconMenu openDirection="bottom-left" iconButtonElement={<IconButton  iconClassName="glyphicon glyphicon-align-justify"/>}>
-                        <MenuItem primaryText="Lists" / >
-                        <MenuItem primaryText="History"  />
-                        <MenuItem primaryText="Collections" />
-                        <MenuItem primaryText="Movies" onItemTouchTap={<Link to="/movies" />} />
-=======
-                iconElementLeft={
-                  <IconMenu openDirection="bottom-right" iconButtonElement={<IconButton iconClassName="glyphicon glyphicon-align-justify" />}>
-                        <MenuItem primaryText="Lists" />
-                        <MenuItem primaryText="History" />
-                        <MenuItem primaryText="Collections" />
-                        <MenuItem primaryText="Movies"  />
->>>>>>> 5de60c406e93c613f41f894bc0c91247a9c22df5
-                  </IconMenu>
-        }/>
+                  <IconMenu
+                    openDirection="bottom-left"
+                    iconButtonElement={
+                      <IconButton
+                        iconClassName="glyphicon glyphicon-align-justify"
+                      />
+                    }
+                    >
+                      <MenuItem primaryText="Lists" / >
+                      <MenuItem primaryText="History"  />
+                      <MenuItem primaryText="Collections" />
+                      <MenuItem primaryText="Movies" onTouchTap={<Link to="/movies" />} />
+                  </IconMenu>}
+        />
         <Link to="/movie-details">Movie Details</Link> <br />
         <Link to="/lists">Show Lists</Link> <br />
         <Link to="/search">Search movie</Link>
