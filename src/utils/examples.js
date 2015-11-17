380e9cf7caf854f7fe460bfe778732aada50d6e4
gotrecillo <sergio.panadero.perez@gmail.com>
2015-11-17 13:13:14 +0100

    default comments added to examples

--- src/utils/examples.js
+++ src/utils/examples.js
@@ -7,6 +7,9 @@ const HISTORY_ID = getId();
 const WATCHLIST_ID = getId();
 const COLLECTION_ID = getId();
 const HARRY_POTTER_LIST_ID = getId();
+const FIRST_COMMENT_ID = getId();
+const SECOND_COMMENT_ID = getId();
+const THIRD_COMMENT_ID = getId();
 
 export const defaultLists = {
   [HISTORY_ID]: {
@@ -45,4 +48,9 @@ export const defaultEntries = {
   [WATCHLIST_ID] : ['234', '432'],
   [COLLECTION_ID] : ['481', '505'],
   [HARRY_POTTER_LIST_ID] : ['546', '234', '62544', '234', '432', '481', '505', '7', '70', '43', '198', '228', '230', '309', '358']
+};
+
+export const defaultComments = {
+  [HARRY_POTTER_LIST_ID] : [{id: FIRST_COMMENT_ID, text: 'Hiiiiii'}, {id: SECOND_COMMENT_ID, text: 'Byeee'}],
+  ['7'] : [{id: THIRD_COMMENT_ID, text: 'This movie is awesome'}]
 };
