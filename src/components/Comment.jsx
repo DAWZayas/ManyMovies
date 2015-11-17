78fed3f89bf461ce7702ff65587ee3abec72293f
gotrecillo <sergio.panadero.perez@gmail.com>
2015-11-18 00:30:53 +0100

    comment dumb component desing finished

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,16 +3,60 @@ import Card from 'material-ui/lib/card/card';
 import CardText from 'material-ui/lib/card/card-text';
 import CardHeader from 'material-ui/lib/card/card-header';
 import CardActions from 'material-ui/lib/card/card-actions';
+import TextField from 'material-ui/lib/text-field';
 import Avatar from 'material-ui/lib/avatar';
 import FontIcon from 'material-ui/lib/font-icon';
 import IconButton from 'material-ui/lib/icon-button';
 import Colors from 'material-ui/lib/styles/colors';
 import { formatDate } from '../utils/date';
+import injectTapEventPlugin from "react-tap-event-plugin";
+injectTapEventPlugin();
+
 
 export default class Comment extends Component {
 
+  constructor(props) {
+    super(props);
+    const { comment } = props;
+    this.state = { editing: false, comment };
+  }
+
+  componentDidUpdate() {
+    if (this.refs.comment) {
+      this.refs.comment.focus();
+      this.refs.comment._getInputNode().select();
+    }
+  }
+
+  _handleTouchEdit() {
+    this.setState({editing: true});
+  }
+
+  _handleTouchEditSubmit() {
+    this._stopEditing();
+  }
+
+  _handleTouchEditCancel() {
+    this._stopEditing();
+  }
+
+  _handleKeyDown(e){
+    // Ctrl + Enter
+    if (e.ctrlKey && e.keyCode === 13){
+      this._stopEditing();
+    // ESC key
+    }else if (e.keyCode === 27){
+      this._stopEditing();
+    }
+  }
+
+  _stopEditing() {
+    this.setState({editing: false});
+  }
+
+
   render() {
-    const { time, text } = this.props.comment;
+    const { time, text, modified } = this.state.comment;
     const userAvatar = (
       <Avatar
         icon={
@@ -26,24 +70,53 @@ export default class Comment extends Component {
       />
     );
 
-    return (
-      <Card>
-        <CardHeader
-          title="You"
-          titleColor={Colors.deepOrange900}
-          titleStyle={{fontWeight: "bold"}}
-          subtitle={formatDate(time)}
-          avatar={userAvatar}
+    const modifiedTime = modified ? <div><small>Edited on {formatDate(modified)}</small></div> : '';
+    const cardBody = this.state.editing ? (
+      <CardText>
+        <TextField
+          onKeyDown={this._handleKeyDown.bind(this)}
+          ref="comment"
+          floatingLabelText="Comment"
+          defaultValue={text}
+          multiLine
+          fullWidth
+          rows={4}
         />
-        <CardText>
-          {text}
-        </CardText>
-        <CardActions style={{float: "right"}}>
+      </CardText>
+      ) : (
+      <CardText>
+        {text}
+        {modifiedTime}
+      </CardText>
+    );
+
+    const cardActions = this.state.editing ? (
+      <CardActions style={{float: "right"}}>
           <IconButton
             iconClassName="material-icons"
-           iconStyle={{color:Colors.grey400}}
+            iconStyle={{color:Colors.green500}}
             tooltipPosition="top-left"
-            tooltip="Edit">
+            tooltip="Edit"
+            onTouchTap={this._handleTouchEditSubmit.bind(this)}>
+            done
+          </IconButton>
+          <IconButton
+            iconClassName="material-icons"
+            iconStyle={{color:Colors.red900}}
+            tooltipPosition="top-left"
+            tooltip="Cancel"
+            onTouchTap={this._handleTouchEditCancel.bind(this)}>
+            clear
+          </IconButton>
+        </CardActions>
+      ) : (
+      <CardActions style={{float: "right"}}>
+          <IconButton
+            iconClassName="material-icons"
+            iconStyle={{color:Colors.grey400}}
+            tooltipPosition="top-left"
+            tooltip="Edit"
+            onTouchTap={this._handleTouchEdit.bind(this)}>
             edit
           </IconButton>
           <IconButton
@@ -54,6 +127,19 @@ export default class Comment extends Component {
             clear
           </IconButton>
         </CardActions>
+    );
+
+    return (
+      <Card>
+        <CardHeader
+          title="You"
+          titleColor={Colors.deepOrange900}
+          titleStyle={{fontWeight: "bold"}}
+          subtitle={formatDate(time)}
+          avatar={userAvatar}
+        />
+        {cardBody}
+        {cardActions}
       </Card>
     );
   }
@@ -67,6 +153,7 @@ Comment.propTypes = {
 Comment.defaultProps = {
   comment:{
     text: 'This movie is awesome',
-    time: new Date()
+    time: new Date(),
+    modified: new Date()
   }
 };
