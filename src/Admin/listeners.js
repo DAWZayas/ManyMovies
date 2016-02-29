6acd194e6087d76f5b915fcbfb893ea97f1645ec
gotrecillo <sergio.panadero.perez@gmail.com>
2016-02-29 22:04:50 +0100

    update queue on admin now reseting

--- src/Admin/listeners.js
+++ src/Admin/listeners.js
@@ -1,8 +1,10 @@
 import { setMovies } from '../Movies/actions/creators';
+import { clearUpdateQueue } from './actions/creators';
 import firebase from '../utils/firebase';
 
 export function registerListeners(dispatch) {
   const ref = firebase.child('movies');
+  dispatch(clearUpdateQueue());
   ref.on('value', snapshot => dispatch(setMovies(snapshot.val())));
 }
 
